acf836ba21442945a4415a7a24830075f2e27ea5
pa001024 <pa001024@qq.com>
2013-04-20 14:23:39 +0800

    Add Login Support

--- src/main/java/net/minecraft/src/NetClientHandler.java
+++ src/main/java/net/minecraft/src/NetClientHandler.java
@@ -162,7 +162,9 @@ public class NetClientHandler extends NetHandler {
 	 */
 	private String sendSessionRequest(String par1Str, String par2Str, String par3Str) {
 		try {
-			URL var4 = new URL("http://session.minecraft.net/game/joinserver.jsp?user=" + urlEncode(par1Str) + "&sessionId=" + urlEncode(par2Str) + "&serverId=" + urlEncode(par3Str));
+			// Start of Nekocraft-patched Code
+			URL var4 = new URL(MySkinConfig.getLoginUrl(urlEncode(par1Str),urlEncode(par2Str),urlEncode(par3Str)));
+			// End of Nekocraft-patched Code
 			BufferedReader var5 = new BufferedReader(new InputStreamReader(var4.openStream()));
 			String var6 = var5.readLine();
 			var5.close();
